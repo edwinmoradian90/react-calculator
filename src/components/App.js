90492166859814bce60111fa81e4ff135070c5f8
edwin moradian <edwin.moradian90@gmail.com>
2020-04-23 15:39:20 -0700

    second-milestone

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+/* eslint-disable react/prefer-stateless-function */
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div>App</div>
+      </div>
+    );
+  }
+}
---
+++ src/components/Display.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Display = props => {
+  const { result } = props;
+  return (
+    <div className="result">
+      {result}
+    </div>
+  );
+};
+
+export default Display;
+
+Display.propTypes = {
+  result: PropTypes.string,
+};
+
+Display.defaultProps = {
+  result: '',
+};
